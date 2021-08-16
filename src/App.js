eaf0a1623b472947216e1ee9b1d5a7be13a0307f
Maggie Hibberd <maggiehibberd@gmail.com>
2021-08-16 14:22:35 +0100

    Testing change in code to make app mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-// import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
 import background from "./img/glenn.png";
+import desktopImage from './img/glenn.png';
+import mobileImage from './img/glennMobile.png';
 import './App.css';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const imageUrl = windowWidth >= 650 ? desktopImage : mobileImage;
+
+  useEffect(() => {
+      const handleWindowResize = () => {
+          setWindowWidth(window.innerWidth);
+      };
+      
+      window.addEventListener('resize', handleWindowResize);
+
+      return () => {
+          window.removeEventListener('resize', handleWindowResize);
+      }
+  }, []);
 
 const currentDate = new Date();
 const currentDayOfMonth = currentDate.getDate();
@@ -19,20 +36,28 @@ const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 console.log(diffTime + " milliseconds");
 console.log(diffDays + " days");
   return (
+    
     <div className="App">
       <header className="App-header">
+     
       <div
-  style={{
-    height:'auto',width:'100%',
-    background: `url('${process.env.PUBLIC_URL}./img/glenn.png')`,
-    size: 'cover'
-  }}
+  
+  // style={{
+  //   height:'auto',width:'100%',
+  //   backgroundImage: `url('${process.env.PUBLIC_URL}./img/glenn.png')`,
+  // }}
 
 >
-<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <h1> Glenn has been dead...{diffDays} days</h1>
-
+<div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
+            <div className="App-content">
+                <h1>Glenn has been dead for...{diffDays} days</h1>
+                
+            </div>
 </div>
+
+{/* <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+    <h1> Glenn has been dead for...{diffDays} days</h1>
+</div> */}
  
 </div>
       {/* <div style={{ backgroundImage:`url(require(${background}))`}}>
