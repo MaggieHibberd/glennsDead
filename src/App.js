c53bedc7548c93d0d784304929f51da736fd1996
Maggie Hibberd <maggiehibberd@gmail.com>
2021-08-18 14:12:12 +0100

    README update and unused code deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
-import background from "./img/glenn.png";
 import desktopImage from './img/glenn.png';
 import mobileImage from './img/glennMobile.png';
 import './App.css';
@@ -13,31 +12,15 @@ function App() {
       const handleWindowResize = () => {
           setWindowWidth(window.innerWidth);
       };
-      
       window.addEventListener('resize', handleWindowResize);
-
       return () => {
           window.removeEventListener('resize', handleWindowResize);
       }
   }, []);
-  const styleObj = {
-    fontSize: 14,
-    color: "red",
-    
-    paddingTop: "100px",
-}
 
-const currentDate = new Date();
-const currentDayOfMonth = currentDate.getDate();
-const currentMonth = currentDate.getMonth(); // Be careful! January is 0, not 1
-const currentYear = currentDate.getFullYear();
-const dateString = currentDayOfMonth + "/" + (currentMonth + 1) + "/" + currentYear;
-
-const today = new Date();
 const date1 = new Date('10/23/2016');
-const date2 = new Date('08/14/2021');
-const date3 = new Date();
-const diffTime = Math.abs(date3 - date1);
+const date2 = new Date();
+const diffTime = Math.abs(date2 - date1);
 const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 console.log(diffTime + " milliseconds");
 console.log(diffDays + " days");
@@ -45,14 +28,7 @@ console.log(diffDays + " days");
     
     <div className="App">
       <header className="App-header">
-     
       <div
-  
-  // style={{
-  //   height:'auto',width:'100%',
-  //   backgroundImage: `url('${process.env.PUBLIC_URL}./img/glenn.png')`,
-  // }}
-
 >
 <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
             <div className="App-content">
@@ -61,21 +37,7 @@ console.log(diffDays + " days");
                 </p>
             </div>
 </div>
-
-{/* <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <h1> Glenn has been dead for...{diffDays} days</h1>
-</div> */}
- 
-</div>
-      {/* <div style={{ backgroundImage:`url(require(${background}))`}}>
-      GLEN IS DEAD
-    </div> */}
-        {/* <img src={glenn} className="glenn" alt="glenn" />
-        <p>
-        
-        </p> */}
-        
-        
+</div> 
       </header>
     </div>
   );
